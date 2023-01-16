600e3df7af300d78e19d3354743027c726e496fa
zzanmin <dlckdals0204@hanyang.ac.kr>
2023-01-16 12:51:47 +0900

    readme.md

--- Baekjoon_Python_Code/10989.py
+++ Baekjoon_Python_Code/10989.py
@@ -1,10 +1,11 @@
-import sys
+""" import sys
 
 N = int(sys.stdin.readline())
 lst = []
 for i in range(N):
     num = int(sys.stdin.readline())
     lst.append(num)
-lst.sort()
 for i in range(N):
-    print(lst[i])
+    print(lst[i]) """
+
+## sort 함수를 사용하면 메모리 초과가 발생함
